8600a22cefa4d9a56ca4c06fc02357874bb017b1
Liam Ellis <liamhellis@gmail.com>
2016-09-09 13:28:20 -0700

    Category controller up and running with index view rendering

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :non_profits
   devise_for :users
+  #Still need to set a root route for Devise
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :categories, :users, :non_profits
 end
